624a005ae34f444584bdccdad3fc5c753c6a386d
Olga Mikhailova <foxx_fire@mail.ru>
2025-08-11 16:55:38 +0300

    Initial Commit

---
+++ iOS-HW20-Olga Mikhailova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-HW20-Olga Mikhailova
+//
+//  Created by FoxxFire on 11.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
