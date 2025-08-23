485fb2dd4418808ccf2f1916737b8d41691ba786
Olga Mikhailova <foxx_fire@mail.ru>
2025-08-23 23:04:53 +0300

    Update Scene and AppDelegate

--- iOS-HW20-Olga Mikhailova/Sources/SceneDelegate.swift
+++ iOS-HW20-Olga Mikhailova/Sources/SceneDelegate.swift
@@ -10,16 +10,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-        
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
+    private let navigationManager = NavigationAppearanceManager()
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = TabBarViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        self.window?.rootViewController = navigationController
-        self.window?.makeKeyAndVisible()
+        navigationManager.setupGlobalAppearance()
+        
+        window?.rootViewController = TabBarViewController()
+        window?.makeKeyAndVisible()
     }
 }
-   
+
+
 
