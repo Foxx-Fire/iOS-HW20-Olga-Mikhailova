b4383525225d8eb2d95ee3362857951d8db8c1c0
Olga Mikhailova <foxx_fire@mail.ru>
2025-09-08 01:43:25 +0300

    Correction after changes in Model

--- iOS-HW20-Olga Mikhailova/AlbumsCollection/AlbumCompositionalLayout.swift
+++ iOS-HW20-Olga Mikhailova/AlbumsCollection/AlbumCompositionalLayout.swift
@@ -10,7 +10,8 @@ import UIKit
 final class AlbumCompositionalLayout {
     
     func createLayout() -> UICollectionViewCompositionalLayout {
-        let layout = UICollectionViewCompositionalLayout { [weak self] sectionIndex, environment in
+        let layout = UICollectionViewCompositionalLayout {
+            [weak self] sectionIndex, environment in
             self?.createSection(for: sectionIndex, environment: environment)
         }
         
@@ -26,11 +27,11 @@ final class AlbumCompositionalLayout {
     }
     
     private func createSection(for sectionIndex: Int, environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? {
-        guard sectionIndex < SectionHeaderModel.allSections.count else {
+        guard sectionIndex < AlbumSection.allSections.count else {
             fatalError("Неизвестная секция: индекс \(sectionIndex) выходит за пределы")
         }
         
-        let sectionType = SectionHeaderModel.allSections[sectionIndex].sectionType
+        let sectionType = AlbumSection.allSections[sectionIndex].type
         
         switch sectionType {
         case .myAlbums:
@@ -39,7 +40,7 @@ final class AlbumCompositionalLayout {
             return createSharedAlbumsSection(environment: environment)
         case .mediaTypes:
             return createMediaTypesSection(environment: environment)
-        case .otherAlbums:
+        case .other:
             return createOtherAlbumsSection(environment: environment)
         }
     }
@@ -206,239 +207,6 @@ final class AlbumCompositionalLayout {
         
         section.decorationItems = [separatorItem]
     }
-    
-//    func createLayout() -> UICollectionViewCompositionalLayout {
-//        let layout = UICollectionViewCompositionalLayout {
-//            sectionIndex, environment in
-//            
-//            guard sectionIndex < SectionHeaderModel.allSections.count else {
-//                fatalError("Неизвестная секция: индекс \(sectionIndex) выходит за пределы")
-//            }
-//            
-//            // Получаем тип секции из модели
-//            let sectionType = SectionHeaderModel.allSections[sectionIndex].sectionType
-//            
-//            switch sectionType {
-//            case .myAlbums:
-//                // сбоку 16
-//                let side: CGFloat = 16
-//                // между двумя колонками в группе 12
-//                let interColumn: CGFloat = 12
-//                // между плитками в колонке 8
-//                let verticalSpacing: CGFloat = 8
-//                
-//                // реальная высота шрифта заголовка
-//                let titleLH = MyAlbumsCell.Constants.descriptionFont.lineHeight
-//                // реальная высота количества изображений
-//                let countLH = MyAlbumsCell.Constants.countFont.lineHeight
-//                // отступ сверху от картинки до стека
-//                let textH = MyAlbumsCell.Constants.stackTopOffset
-//                + MyAlbumsCell.Constants.stackSpacing
-//                + titleLH + countLH
-//                
-//                // ширина без боковых insets
-//                let contentW = environment.container.effectiveContentSize.width - side * 2
-//                // ширина одной колонки (2 колонки и зазор)
-//                let columnW = (contentW - interColumn) / 2
-//                // высота ячейки: квадратная картинка + текст
-//                let itemH = columnW + textH
-//                // высота вертикальной группы из 2 items
-//                let vGroupH = itemH * 2 + verticalSpacing
-//                
-//                // 1. Настройка элемента
-//                let itemSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1), // 100% ширины колонки
-//                    heightDimension: .absolute(itemH) // фиксированная высота
-//                )
-//                
-//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//                
-//                // 2. Вертикальная группа из 2 элементов (столбец)
-//                let verticalGroupSize = NSCollectionLayoutSize(
-//                    widthDimension: .absolute(columnW), // = ширина колонки
-//                    heightDimension: .absolute(vGroupH) // = сумма высот items + зазор
-//                )
-//                
-//                let verticalGroup = NSCollectionLayoutGroup.vertical(
-//                    layoutSize: verticalGroupSize,
-//                    subitems: [item, item] // Два элемента в столбце
-//                )
-//                
-//                // 3. Основная горизонтальная группа из 2 вертикальных групп
-//                let planeGroupSize = NSCollectionLayoutSize(
-//                    widthDimension: .absolute(contentW), // = вся доступная ширина секции
-//                    heightDimension: .absolute(vGroupH) // = высота колонки
-//                )
-//                
-//                let group = NSCollectionLayoutGroup.horizontal(
-//                    layoutSize: planeGroupSize,
-//                    subitems: [verticalGroup, verticalGroup] // Два столбца
-//                )
-//                group.interItemSpacing = .fixed(interColumn) // зазор между колонками
-//                
-//                // 4. Настройка секции
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.orthogonalScrollingBehavior = .continuous
-//                section.interGroupSpacing = interColumn
-//                section.contentInsets = NSDirectionalEdgeInsets(
-//                    top: .zero,
-//                    leading: side,
-//                    bottom: side,
-//                    trailing: side
-//                )
-//                
-//                // 4. Настройка хэдера для секции
-//                let headerSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(44)
-//                )
-//                
-//                let header = NSCollectionLayoutBoundarySupplementaryItem(
-//                    layoutSize: headerSize,
-//                    elementKind: UICollectionView.elementKindSectionHeader,
-//                    alignment: .top
-//                )
-//                
-//                section.boundarySupplementaryItems = [header]
-//                
-//                // Разделитель
-//                let separatorItem = NSCollectionLayoutDecorationItem.background(
-//                    elementKind: SectionSeparatorView.identifier
-//                )
-//                
-//                separatorItem.contentInsets = NSDirectionalEdgeInsets(
-//                    top: 0,
-//                    leading: 2,
-//                    bottom: 0,
-//                    trailing: 2
-//                )
-//                
-//                section.decorationItems = [separatorItem]
-//                
-//                return section
-//                
-//            case .sharedAlbums:
-//                let side: CGFloat = 16 // бок
-//                let titleLH = SharedAlbumsCell.Constants.descriptionFont.lineHeight
-//                let labelExtra = SharedAlbumsCell.Constants.stackTopOffset
-//                + SharedAlbumsCell.Constants.stackSpacing + titleLH * 2
-//                
-//                let contentW = environment.container.effectiveContentSize.width - side * 2 // доступная ширина секции
-//                let groupW = contentW * 0.48  // ширина одной карточки (2 рядом, с зазором)
-//                let itemH = groupW + labelExtra  // высота карточки (квадрат + подпись)
-//                
-//                // 1. Настройка элемента
-//                let item = NSCollectionLayoutItem(
-//                    layoutSize: .init(
-//                        widthDimension: .fractionalWidth(1.0), // заполняет группу по ширине
-//                        heightDimension: .absolute(itemH)
-//                    )
-//                )
-//                item.contentInsets = .zero   // внутренних insets у item нет
-//                
-//                // 2. Группа для горизонтального скролла
-//                let groupSize = NSCollectionLayoutSize(
-//                    widthDimension: .absolute(groupW),
-//                    heightDimension: .absolute(itemH)
-//                )
-//                
-//                let group = NSCollectionLayoutGroup.horizontal(
-//                    layoutSize: groupSize,
-//                    subitems: [item]
-//                )
-//                
-//                // 3. Настройка секции
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
-//                section.interGroupSpacing = side // Расстояние между группами
-//                section.contentInsets = NSDirectionalEdgeInsets(
-//                    top: side,
-//                    leading: side,
-//                    bottom: side,
-//                    trailing: side
-//                )
-//                
-//                // 4. Добавляем хэдер
-//                let headerSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(44)
-//                )
-//                
-//                let header = NSCollectionLayoutBoundarySupplementaryItem(
-//                    layoutSize: headerSize,
-//                    elementKind: UICollectionView.elementKindSectionHeader,
-//                    alignment: .top
-//                )
-//                
-//                section.boundarySupplementaryItems = [header]
-//                
-//                // Разделитель
-//                let separatorItem = NSCollectionLayoutDecorationItem.background(
-//                    elementKind: SectionSeparatorView.identifier
-//                )
-//                
-//                separatorItem.contentInsets = NSDirectionalEdgeInsets(
-//                    top: 0,
-//                    leading: 2,
-//                    bottom: 0,
-//                    trailing: 2
-//                )
-//                
-//                section.decorationItems = [separatorItem]
-//                
-//                return section
-//                
-//            case .mediaTypes, .otherAlbums:
-//                let side: CGFloat = 16 // бок
-//                
-//                let itemSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(54)
-//                )
-//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//                
-//                let groupSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(44)
-//                )
-//                let group = NSCollectionLayoutGroup.horizontal(
-//                    layoutSize: groupSize,
-//                    subitems: [item]
-//                )
-//                
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.contentInsets = NSDirectionalEdgeInsets(
-//                    top: .zero,
-//                    leading: side,
-//                    bottom: .zero,
-//                    trailing: side
-//                )
-//                
-//                let headerSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(44)
-//                )
-//                
-//                let header = NSCollectionLayoutBoundarySupplementaryItem(
-//                    layoutSize: headerSize,
-//                    elementKind: UICollectionView.elementKindSectionHeader,
-//                    alignment: .top
-//                )
-//                
-//                section.boundarySupplementaryItems = [header]
-//                return section
-//            }
-//        }
-//        // глобальная конфигурация layout
-//        let config = UICollectionViewCompositionalLayoutConfiguration()
-//        config.interSectionSpacing = 0 // между секциями
-//        layout.configuration = config // применяем конфиг
-//        layout.register( // решистрация сепаратора
-//            SectionSeparatorView.self,
-//            forDecorationViewOfKind: SectionSeparatorView.identifier
-//        )
-//        return layout
-//    }
 }
 
 // MARK: - Constants
